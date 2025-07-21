9c47ad7c477d36426849bb14768f13a28f15ce2b
syed zohaib <zohaibsyed914@gmail.com>
2025-07-22 00:52:12 +0500

    working start

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+// import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { lazy, Suspense } from "react"
+import Loader from "./components/Loader"
+
+
+const Home = lazy(() => import("./pages/home"))
+const Search = lazy(() => import("./pages/search"))
+const Cart = lazy(() => import("./pages/cart"))
+
+
+const App = () => {
+  return (
+    <>
+      <Router>
+        {/* Headers */}
+        <Suspense fallback={<Loader />}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/search" element={<Search />} />
+            <Route path="/cart" element={<Cart />} />
+          </Routes>
+        </Suspense>
+      </Router>
+    </>
+  )
+}
+
+export default App
