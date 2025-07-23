f576d1e5250c991fd3e28c5e7895ff150f1dee4f
syed zohaib <zohaibsyed914@gmail.com>
2025-07-24 00:49:37 +0500

    add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 // import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import { lazy, Suspense } from "react"
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom"
+import Header from "./components/Header"
 import Loader from "./components/Loader"
 
 
@@ -37,6 +38,7 @@ const App = () => {
     <>
       <Router>
         {/* Headers */}
+        <Header />
         <Suspense fallback={<Loader />}>
           <Routes>
             <Route path="/" element={<Home />} />
